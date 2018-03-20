7f8f964f7fef27a8fc320cb951559e3789ee183f
yzhbankov <yzhbankov@redpointpositioning.com>
2018-03-20 16:41:41 +0000

    add heroes component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. nice' },
+  { id: 12, name: 'Marco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Square pents' },
+  { id: 15, name: 'Chear man' },
+  { id: 16, name: 'Snow man' },
+  { id: 17, name: 'Bet man' },
+  { id: 18, name: 'Sub zero' },
+  { id: 19, name: 'Terminator' },
+  { id: 20, name: 'Robocop' }
+];
